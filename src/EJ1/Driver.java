7e7d64b75fa90c5656af79199e84140bc40b045c
Nicolas Garcia <nicodev1229@gmail.com>
2022-12-15 13:39:27 -0300

    actu

--- src/EJ1/Driver.java
+++ src/EJ1/Driver.java
@@ -1,29 +1,34 @@
 package EJ1;
-
 import com.mysql.jdbc.Connection;
 import com.mysql.jdbc.Statement;
-
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 
 public class Driver {
     public static void main(String[] args) {
 
+        String url = "jdbc:mysql://localhost:3307/nba?useSSL=false";
+        String user = "root";
+        String pass = "root";
+
         try {
 
-            Connection conexion = (Connection) DriverManager.getConnection("jdbc:mysql://localhost:3307/nba", "root", "root");
+            Connection conexion = (Connection) DriverManager.getConnection(url, user, pass);
             Statement sentencia = (Statement) conexion.createStatement();
-            ResultSet resultado = sentencia.executeQuery("SELECT * FROM jugador LIMIT 10");
-            System.out.println("Nos conectamos joyaaa");
+            ResultSet resultado = sentencia.executeQuery("SELECT * FROM jugador limit 10");
             System.out.println();
 
             while (resultado.next()) {
-                System.out.println("Jugador: " + resultado.getString("nombre"));
+                System.out.println("Nombre jugador: " + resultado.getString("nombre") + " -- " +"Procedencia: " + resultado.getString("procedencia"));
             }
+
+            conexion.close();
+            sentencia.close();
+            resultado.close();
 
         } catch (Exception e) {
             e.printStackTrace();
-
         }
+
     }
 }
